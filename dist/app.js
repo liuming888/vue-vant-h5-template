b4832062254d77d7a683193be19552d95d3c55d3
liuming <liuming@batmobi.net>
2019-03-12 10:46:53 +0800

    refactor: 砍价中的商品列表联调调整

--- dist/app.js
+++ dist/app.js
@@ -92,7 +92,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5ca05b75ee5eb4c7a1f6";
+/******/ 	var hotCurrentHash = "6707804dbaf843e17935";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
