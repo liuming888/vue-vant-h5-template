c20f9273b700726dc2391ceb6024f99f3b469ffa
liuming <liuming@batmobi.net>
2019-03-13 12:56:44 +0800

    refactor: 注释掉quicklink

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import quicklink from 'quicklink/dist/quicklink.mjs';
+// import quicklink from 'quicklink/dist/quicklink.mjs';
 Vue.use(Router);
 
 const curRouter = new Router({
