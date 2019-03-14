8ece765a7ac99b818b43437124099013019159d3
tangjinping <tangjinping@batmobi.net>
2019-03-14 19:14:23 +0800

    test sonar

---
+++ test2.js
@@ -0,0 +1,45 @@
+console.log('111111111111111', process.env.NODE_ENV);
+function getUrl(NODE_ENV) {
+    let url = '';
+    switch (NODE_ENV) {
+        case 'mock':
+            url = 'http://doclever.batmobi.cn/mock/5c7f95350fcb632d8754e6bf/api';
+            break;
+        case 'development':
+            // 锦涛
+            // url = 'http://192.168.4.128:8024/api';
+            // 阳
+            // url = 'http://192.168.4.32:8024/api';
+            // url = 'https://dev-ht-zdd-api.batmobi.net/api';
+            url = 'https://test-ht-zdd-api.batmobi.net/api';
+            break;
+        case 'test':
+            // url = 'http://127.0.0.1:7001';
+            break;
+        case 'production':
+            // url = 'http://127.0.0.1:9078';
+            break;
+    }
+    return url;
+}
+
+module.exports = {
+    lintOnSave: false,
+    productionSourceMap: false,
+    devServer: {
+        // disableHostCheck: true,
+        proxy: {
+            '/api': {
+                target: getUrl(process.env.NODE_ENV),
+
+                ws: true,
+	 email: xx@batmobi.net,
+                changeOrigin: true,
+
+                pathRewrite: {
+                    '^/api': '/', // 这里理解成用‘/api’代替target里面的地址，后面组件中我们掉接口时直接用api代替 比如我要调用'http://40.00.100.100:3002/user/add'，直接写‘/api/user/add’即可
+                },
+            },
+        },
+    }
+};
