f12175ca695d21589e336b48450636d67c36c1d4
liuming <liuming@batmobi.net>
2019-03-19 11:38:57 +0800

    test

--- src/utils/api/config.js
+++ src/utils/api/config.js
@@ -5,7 +5,6 @@ export default {
             case 'mock':
                 url = '';
             case 'development':
-                // url = 'http://dev-ht-zdd-api.batmobi.net';
                 break;
             case 'dev': // 开发域名环境
                url = 'https://dev-ht-zdd-api.istarbuy.com/';
@@ -13,8 +12,8 @@ export default {
                 // url = 'http://192.168.4.32:8024';
                 break;
             case 'test':
-                url = 'https://test-ht-zdd-api.batmobi.net';
-                // url = 'http://test-ht-zdd-api.batmobi.cn:43321';
+                // url =' https://test-ht-zdd-api.istarbuy.com/';
+                url ='https://test-zdd-api.istarbuy.cn:8443/';
                 break;
             case 'production':
                 url = 'https://api.istarbuy.com';
