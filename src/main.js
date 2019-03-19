51ad0c9224d8d2783eb0088bfa0905899fabbea8
liuming <liuming@batmobi.net>
2019-03-19 14:43:49 +0800

    refactor: 细节调整

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router/index.js';
 import store from './store/index.js';
-import $request from '@/utils/api/request.js';
+// import $request from '@/utils/api/request.js';
 import * as $util from './utils/util.js';
 import './utils/faceBookSdk.js';
 import { Toast, Popup, Lazyload } from 'vant';
@@ -12,7 +12,7 @@ Vue.use(Lazyload);
 
 Vue.prototype.$curStore = store;
 Vue.prototype.$util = $util;
-Vue.prototype.$request = $request;
+// Vue.prototype.$request = $request;
 
 Vue.config.productionTip = false;
 
