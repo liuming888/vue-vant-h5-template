f264f642a505f2dc11fccea8f8fe397f6ed88f58
liuming <liuming@batmobi.net>
2019-03-20 16:59:32 +0800

    refactor: FB配置引入问题修复

--- src/mixins/fbInit.js
+++ src/mixins/fbInit.js
@@ -1,10 +1,10 @@
+import { FBConfig } from '@/config/index.js';
 export default {
     beforeCreate() {
         if (process.env.VUE_APP_ENV == 'development' || process.env.VUE_APP_ENV == 'mock' || window.FB) {
             return;
         }
         require('@/utils/faceBookSdk.js');
-        const { FBConfig } = require('@/config/index.js');
         window.fbAsyncInit = async function() {
             FB.init(FBConfig);
             FB.AppEvents.logPageView();
