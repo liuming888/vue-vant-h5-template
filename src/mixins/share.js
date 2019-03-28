dc1dcc74535dc34472471b695a3abe6ff5538dbe
liuming <liuming@batmobi.net>
2019-03-29 00:39:56 +0800

    refactor: 分享加上分享标题

--- src/mixins/share.js
+++ src/mixins/share.js
@@ -2,7 +2,7 @@
  * @Description: 分享组件相关的公共方法
  * @LastEditors: liuming
  * @Date: 2019-03-10 10:35:30
- * @LastEditTime: 2019-03-28 11:39:15
+ * @LastEditTime: 2019-03-29 00:33:06
  */
 import { FBConfig } from '@/config/index.js';
 import Clipboard from 'clipboard';
@@ -20,7 +20,7 @@ export default {
          * @description:  点击复制按钮后
          */
         mx_copyLink() {
-            console.log("6666666666666")
+            console.log('6666666666666');
             let vm = this;
             let clipboard = vm.mx_copyBtn;
             clipboard.on('success', function() {
@@ -38,8 +38,9 @@ export default {
             const appId = FBConfig.appId; // FB的appid
             console.log('appId: ', appId);
             const link = this.shareInfo.share_url; // 分享的链接（必须和FB应用设置的一致）
+            var t = this.shareInfo.share_title;
             // const link = 'https://liuming.mynatapp.cc/forBargain'; // 测试
-            window.location.href = `fb-messenger://share/?link=${link}/&app_id=${appId}`;
+            window.location.href = `fb-messenger://share/?link=${encodeURIComponent(t)+'          ' + link}/&app_id=${appId}`;
         },
         /**
          * @description: 分享到whatsapp
@@ -48,7 +49,7 @@ export default {
             var t = this.shareInfo.share_title;
             const link = encodeURIComponent('\n\n' + this.shareInfo.share_url); // 分享的链接
             console.log('this.shareInfo.share_url: ', this.shareInfo.share_url);
-            window.location.href = 'whatsapp://send?text=' + encodeURIComponent(t) + link + '&via=lopscoop';
+            window.location.href = 'whatsapp://send?text=' + encodeURIComponent(t)+'          ' + link + '&via=lopscoop';
         },
         /**
          * @description: 分享到LINE
@@ -56,8 +57,9 @@ export default {
         mx_shareLine() {
             //  分享第一种没限制服务器ip地区的（分享文字为主，好像也可以显示图片了）
             const link = encodeURIComponent(this.shareInfo.share_url); // 分享的链接
+            var t = this.shareInfo.share_title;
             // const link = encodeURIComponent('https://liuming.mynatapp.cc/forBargain'); // 测试
-            window.location.href = `line://msg/text/${link}`;
+            window.location.href = `line://msg/text/${encodeURIComponent(t)+'          ' + link}`;
 
             // 分享第二种（得翻墙到日本等）
             //  window.location.href = `https://social-plugins.line.me/lineit/share?url=${link}`;
@@ -66,11 +68,12 @@ export default {
          * @description: 分享到FB
          */
         async mx_shareFacebook() {
-             const link =this.shareInfo.share_url; // 分享的链接
+            const link = this.shareInfo.share_url; // 分享的链接
+            var t = this.shareInfo.share_title;
             // const link = 'https://liuming.mynatapp.cc/forBargain?a=1'; // 测试
             // const quote = this.shareInfo.quote;
             // const hashtag=this.shareInfo.hashtag;
-            let result = await window.$faceBookApi.shareFB(link/* , quote */ /* ,hashtag */);
+            let result = await window.$faceBookApi.shareFB(link,t /* , quote */ /* ,hashtag */);
             if (result) {
                 // 分享成功
             } else {
