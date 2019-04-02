3093aaec5223dd076ec4b44ce17cc8e463009625
liuming <liuming@batmobi.net>
2019-04-02 19:55:38 +0800

    refactor: FB初始化时机变为App.vue里触发

--- src/mixins/fbInit.js
+++ src/mixins/fbInit.js
@@ -1,14 +1,14 @@
-import { FBConfig } from '@/config/index.js';
+// import { FBConfig } from '@/config/index.js';
 export default {
-    beforeCreate() {
-        if (process.env.VUE_APP_ENV == 'development' || process.env.VUE_APP_ENV == 'mock' || window.FB) {
-            return;
-        }
-        require('@/utils/faceBookSdk.js');
-        window.fbAsyncInit = async function() {
-            FB.init(FBConfig);
-            FB.AppEvents.logPageView();
-            console.warn('已经初始化FB了');
-        };
-    },
+    // beforeCreate() {
+    //     if (process.env.VUE_APP_ENV == 'development' || process.env.VUE_APP_ENV == 'mock' || window.FB) {
+    //         return;
+    //     }
+    //     require('@/utils/faceBookSdk.js');
+    //     window.fbAsyncInit = async function() {
+    //         FB.init(FBConfig);
+    //         FB.AppEvents.logPageView();
+    //         console.warn('已经初始化FB了');
+    //     };
+    // },
 };
