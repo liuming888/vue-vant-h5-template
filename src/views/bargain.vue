12e58483d1edfb927d5c32b4bb4b58a62b83bb6c
liuming <liuming@batmobi.net>
2019-04-15 19:07:29 +0800

    打个dev包

--- src/views/bargain.vue
+++ src/views/bargain.vue
@@ -236,9 +236,6 @@ export default {
   },
   methods: {
     async init() {
-      this.initSpuInfo();
-      this.initSpuList();
-
       const {
         relationId,
         showShareEarningEntry,
@@ -272,8 +269,8 @@ export default {
         }
       }
 
-      // this.initSpuInfo();
-      // this.initSpuList();
+      this.initSpuInfo();  // 必须有spu_id
+      this.initSpuList();
     },
     async initShareInfo(relationId) {
       let result = await shareInfo({ relation_id: relationId });
