abbc699568d918f53030f13a1300caaccc1290f0
liuming <liuming@batmobi.net>
2019-04-19 16:19:14 +0800

    refactor: 国家编码对应文字名做多语言

--- src/language/index.js
+++ src/language/index.js
@@ -1,21 +1,22 @@
 // import Vue from 'vue';
 // import VueI18n from 'vue-i18n'
-import en from './lib/en.js';
+import us from './lib/us.js';
 import axios from 'axios';
 // Vue.use(VueI18n);
-const loadedLanguages = ['en']; // 我们的预装默认语言
+const loadedLanguages = ['us']; // 我们的预装默认语言
 
 export const i18n = new VueI18n({
-    locale: 'en', // 设置语言环境
-    fallbackLocale: 'en',  // 回退的默认语言
+    locale: 'us', // 设置语言环境
+    fallbackLocale: 'us',  // 回退的默认语言
     messages: {
-        en,
+        us,
     },
 });
 
 function setI18nLanguage(lang) {
     i18n.locale = lang;
     axios.defaults.headers.common['Accept-Language'] = lang;
+    // axios.defaults.headers.common['Accept-CurLang'] = lang;  // 自定义字段
     document.querySelector('html').setAttribute('lang', lang);
     return lang;
 }
