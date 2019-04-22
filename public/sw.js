30388ca9c2d8ece0d29fe859c531d8a7daec6b89
liuming <liuming@batmobi.net>
2019-04-22 22:06:57 +0800

    refactor: 注释掉缓存pwa的代码，发现第三方如FB，google请求也算到了

--- public/sw.js
+++ public/sw.js
@@ -10,6 +10,7 @@ self.addEventListener('install', function(e) {
     e.waitUntil(cacheOpenPromise);
 });
 
+// 简单缓存
 // self.addEventListener('fetch', function(e) {
 //     // 如果有cache则直接返回，否则通过fetch请求
 //     e.respondWith(
@@ -24,46 +25,48 @@ self.addEventListener('install', function(e) {
 //             })
 //     );
 // });
-self.addEventListener('fetch', function(e) {
-    // 需要缓存的xhr请求
-    var cacheRequestUrls = [
-        /* '/my/Tutorial' */
-    ];
-    // console.log('现在正在请求：' + e.request.url);
 
-    // 判断当前请求是否需要缓存
-    var needCache = cacheRequestUrls.some(function(url) {
-        return e.request.url.indexOf(url) > -1;
-    });
+// 复杂缓存
+// self.addEventListener('fetch', function(e) {
+//     // 需要缓存的xhr请求
+//     var cacheRequestUrls = [
+//         /* '/my/Tutorial' */
+//     ];
+//     // console.log('现在正在请求：' + e.request.url);
 
-    /**** 这里是对XHR数据缓存的相关操作 ****/
-    if (needCache) {
-        // 需要缓存
-        // 使用fetch请求数据，并将请求结果clone一份缓存到cache
-        // 此部分缓存后在browser中使用全局变量caches获取
-        caches.open(apiCacheName).then(function(cache) {
-            return fetch(e.request).then(function(response) {
-                cache.put(e.request.url, response.clone());
-                return response;
-            });
-        });
-    } else {
-        /* ******************************* */
-        // 非api请求，直接查询cache
-        // 如果有cache则直接返回，否则通过fetch请求
-        e.respondWith(
-            caches
-                .match(e.request)
-                .then(function(cache) {
-                    return cache || fetch(e.request);
-                })
-                .catch(function(err) {
-                    console.log(err);
-                    return fetch(e.request);
-                })
-        );
-    }
-});
+//     // 判断当前请求是否需要缓存
+//     var needCache = cacheRequestUrls.some(function(url) {
+//         return e.request.url.indexOf(url) > -1;
+//     });
+
+//     /**** 这里是对XHR数据缓存的相关操作 ****/
+//     if (needCache) {
+//         // 需要缓存
+//         // 使用fetch请求数据，并将请求结果clone一份缓存到cache
+//         // 此部分缓存后在browser中使用全局变量caches获取
+//         caches.open(apiCacheName).then(function(cache) {
+//             return fetch(e.request).then(function(response) {
+//                 cache.put(e.request.url, response.clone());
+//                 return response;
+//             });
+//         });
+//     } else {
+//         /* ******************************* */
+//         // 非api请求，直接查询cache
+//         // 如果有cache则直接返回，否则通过fetch请求
+//         e.respondWith(
+//             caches
+//                 .match(e.request)
+//                 .then(function(cache) {
+//                     return cache || fetch(e.request);
+//                 })
+//                 .catch(function(err) {
+//                     console.log(err);
+//                     return fetch(e.request);
+//                 })
+//         );
+//     }
+// });
 
 // 监听activate事件，激活后通过cache的key来判断是否更新cache中的静态资源
 self.addEventListener('activate', function(e) {
