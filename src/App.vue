956a455534c2c787dabdc72b6ba2e55994ebff23
liuming <liuming@batmobi.net>
2019-05-04 20:50:30 +0800

    打个dev包

--- src/App.vue
+++ src/App.vue
@@ -83,47 +83,47 @@ export default {
     /**
      * @description: 初始化FB
      */
-    // initFB() {
-    //   require("@/utils/faceBookSdk.js");
-    //   const vm = this;
-    //   window.fbAsyncInit = async function() {
-    //     FB.init(FBConfig);
-    //     FB.AppEvents.logPageView();
-    //     console.warn("已经初始化FB了");
+    initFB() {
+      require("@/utils/faceBookSdk.js");
+      const vm = this;
+      window.fbAsyncInit = async function() {
+        FB.init(FBConfig);
+        FB.AppEvents.logPageView();
+        console.warn("已经初始化FB了");
 
 
-    //     /* let checkResInfo = await window.$faceBookApi.checkFBLoginState();
-    //     console.log("是否已经登录了checkResInfo: ", checkResInfo);
-    //     if (checkResInfo) {
-    //       let {
-    //         authResponse: { accessToken },
-    //         id,
-    //         name,
-    //         pic_square
-    //       } = checkResInfo;
-    //       let result = await login({
-    //         tp_id: id,
-    //         tp_token: accessToken,
-    //         tp_type: 1
-    //       });
+        /* let checkResInfo = await window.$faceBookApi.checkFBLoginState();
+        console.log("是否已经登录了checkResInfo: ", checkResInfo);
+        if (checkResInfo) {
+          let {
+            authResponse: { accessToken },
+            id,
+            name,
+            pic_square
+          } = checkResInfo;
+          let result = await login({
+            tp_id: id,
+            tp_token: accessToken,
+            tp_type: 1
+          });
 
-    //       if (result) {
-    //         let userInfo = result.data;
-    //         vm.$store.commit("setUserInfo", userInfo);
-    //         localStorage.setItem("userInfo", JSON.stringify(userInfo));
-    //         axios.defaults.headers.common["user_id"] = userInfo.user_id;
-    //         axios.defaults.headers.common["access_token"] =
-    //           userInfo.access_token;
-    //         console.log("1111111111111", vm.$store.state.userInfo);
-    //         console.log("222222222222", localStorage.getItem("userInfo"));
-    //       }
-    //     } else {
-    //       // 直接弹起会被浏览器阻止
-    //       // let loginInfo=await  window.$faceBookApi.loginFB();
-    //       // console.log('loginInfo: ', loginInfo);
-    //     } */
-    //   };
-    // },
+          if (result) {
+            let userInfo = result.data;
+            vm.$store.commit("setUserInfo", userInfo);
+            localStorage.setItem("userInfo", JSON.stringify(userInfo));
+            axios.defaults.headers.common["user_id"] = userInfo.user_id;
+            axios.defaults.headers.common["access_token"] =
+              userInfo.access_token;
+            console.log("1111111111111", vm.$store.state.userInfo);
+            console.log("222222222222", localStorage.getItem("userInfo"));
+          }
+        } else {
+          // 直接弹起会被浏览器阻止
+          // let loginInfo=await  window.$faceBookApi.loginFB();
+          // console.log('loginInfo: ', loginInfo);
+        } */
+      };
+    },
     /**
      * @description: 刷新下登录token
      */
